a2e94a16c414a96002101bebe16a31eff2794084
neoxia <neoxia@tab.home>
2023-11-17 12:58:31 +0100

    added shell cmd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
   apps: [
     {
       name: 'back',
-      script: 'srv/back/current/dist/main.js',
+      script: 'srv/back/current/start_back.sh',
       cwd: '/',
       time: true,
       instances: 1,
@@ -32,7 +32,7 @@ module.exports = {
       repo: 'git@github.com:mohamedBENKHOUYA/unionista-back.git',
       path: '/srv/back',
       'post-deploy':
-        'npm install && ls -a && npm run build && pm2 kill && pm2 reload ecosystem.config.js --env preprod --update-env && pm2 save && docker compose up --build',
+        'npm install && ls -a && npm run build && pm2 kill && pm2 reload ecosystem.config.js --env preprod --update-env && pm2 save',
     },
   },
 };
