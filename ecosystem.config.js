99e24113a7b2ba152d727c72ada87beb663e2520
neoxia <neoxia@tab.home>
2023-11-17 13:07:57 +0100

    removed script file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
   apps: [
     {
       name: 'back',
-      script: 'docker compose up --build',
+
       cwd: '/',
       time: true,
       instances: 1,
@@ -32,7 +32,7 @@ module.exports = {
       repo: 'git@github.com:mohamedBENKHOUYA/unionista-back.git',
       path: '/srv/back',
       'post-deploy':
-        'npm install && ls -a && npm run build && pm2 kill && pm2 reload ecosystem.config.js --env preprod --update-env && pm2 save',
+        'npm install && ls -a && npm run build && pm2 kill && pm2 reload ecosystem.config.js --env preprod --update-env && pm2 save && docker compose up --build',
     },
   },
 };
