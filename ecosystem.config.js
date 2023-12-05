c36ef471522dabf714cb1c5342bec1edf9e81758
neoxia <mbk@gmail.com>
2023-12-05 21:40:47 +0100

    removed known_hosts in deploy config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -21,24 +21,6 @@ module.exports = {
         TYPEORM_MIGRATIONS_DIR: 'scripts/migrations',
       },
     },
-    {
-      name: 'front',
-      script: '/srv/front/start_front.sh',
-      cwd: '/srv/front',
-      time: true,
-      instances: 1,
-      autorestart: true,
-      max_restarts: 50,
-      watch: false,
-      max_memory_restart: '1G',
-      env_preprod: {
-        PORT: 3001,
-        NODE_ENV: 'preprod',
-        JWT_ISSUER: 'UnionistaShop',
-        JWT_TTL: '12h',
-        TYPEORM_MIGRATIONS_DIR: 'scripts/migrations',
-      },
-    },
   ],
   deploy: {
     preprod: {
@@ -50,7 +32,7 @@ module.exports = {
       repo: 'git@github.com:mohamedBENKHOUYA/unionista-back.git',
       path: '/srv/back',
       'post-deploy':
-        'npm install && ls -a && npm run build && pm2 kill && pm2 reload ecosystem.config.js --env preprod --update-env && pm2 save',
+        'ls -a && npm run build && pm2 kill && pm2 reload ecosystem.config.js --env preprod --update-env && pm2 save',
     },
   },
 };
