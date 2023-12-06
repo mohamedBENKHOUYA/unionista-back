cc5a408638869704d637b32ec7ed6b321f77f518
neoxia <mbk@gmail.com>
2023-12-06 10:59:29 +0100

    create network if do not exist

--- start_back.sh
+++ start_back.sh
@@ -1,4 +1,3 @@
 docker compose --file /srv/back/current/docker-compose.yml down --remove-orphans
-docker network remove unionistashop_network
-docker network create --driver bridge unionistashop_network
+docker network inspect unionistashop_network >/dev/null || docker network create --driver bridge unionistashop_network
 docker compose --file /srv/back/current/docker-compose.yml up --build
