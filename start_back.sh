174b2e3524f7ac4c28b607f49f7f2a8c5ff1caa3
neoxia <mbk@gmail.com>
2023-12-06 11:17:51 +0100

    create network

--- start_back.sh
+++ start_back.sh
@@ -1,3 +1,7 @@
 docker compose --file /srv/back/current/docker-compose.yml down --remove-orphans
 #docker network inspect unionistashop_network >/dev/null || docker network create --driver bridge unionistashop_network
+NETWORK_NAME=unionistashop_network
+if [ -z $(docker network ls --filter name=^${NETWORK_NAME}$ --format="{{ .Name }}") ] ; then 
+     docker network --driver bridge create ${NETWORK_NAME} ; 
+fi
 docker compose --file /srv/back/current/docker-compose.yml up --build
