5becfd9e4688def7d487fd6b0d5c1ffb5372ec73
neoxia <mbk@gmail.com>
2023-12-06 10:47:51 +0100

    created unionistashop network

--- start_back.sh
+++ start_back.sh
@@ -1,2 +1,4 @@
 docker compose --file /srv/back/current/docker-compose.yml down --remove-orphans
+docker network remove unionistashop_network
+docker network create --driver bridge unionistashop_network
 docker compose --file /srv/back/current/docker-compose.yml up --build
