ce89b510378b5a3b0cae0ce3740a421f788113a9
neoxia <mbk@gmail.com>
2023-12-30 22:56:15 +0100

    not building each time CI running

--- start_back.sh
+++ start_back.sh
@@ -3,4 +3,4 @@ NETWORK_NAME=unionistashop_network
 if [ -z $(docker network ls --filter name=^${NETWORK_NAME}$ --format="{{ .Name }}") ] ; then 
      docker network create --driver bridge ${NETWORK_NAME} ; 
 fi
-docker compose --file /srv/back/current/docker-compose.yml up --build
+docker compose --file /srv/back/current/docker-compose.yml up
