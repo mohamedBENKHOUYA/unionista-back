df5dc260e6e9d16d6335f50e9cf9d9ea0556823c
neoxia <mbk@gmail.com>
2024-01-01 17:57:28 +0100

    without remove orphans

--- start_back.sh
+++ start_back.sh
@@ -1,4 +1,4 @@
-docker compose --file /srv/back/current/docker-compose.yml down --remove-orphans
+docker compose --file /srv/back/current/docker-compose.yml down
 NETWORK_NAME=unionistashop_network
 if [ -z $(docker network ls --filter name=^${NETWORK_NAME}$ --format="{{ .Name }}") ] ; then 
      docker network create --driver bridge ${NETWORK_NAME} ; 
