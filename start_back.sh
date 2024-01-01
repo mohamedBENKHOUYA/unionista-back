9c82dc6fb1f6364cf5edd2c8638a254db98e3e93
neoxia <mbk@gmail.com>
2024-01-01 19:04:23 +0100

    remove orphans if postgres do not exist

--- start_back.sh
+++ start_back.sh
@@ -4,6 +4,7 @@ if [ -z $(docker network ls --filter name=^${NETWORK_NAME}$ --format="{{ .Name }
      docker network create --driver bridge ${NETWORK_NAME} ; 
 fi
 if [ -z $(docker ps --filter name=postgres --format="{{ .Names }}") ] ; then 
+     docker compose --file /srv/back/current/docker-compose.yml down --remove-orphans
      docker compose --file /srv/back/current/docker-compose.yml --profile static up --build
 else 
      docker compose --file /srv/back/current/docker-compose.yml up
