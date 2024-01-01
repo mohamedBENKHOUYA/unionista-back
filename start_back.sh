99c53e1cc7b47601eb46525108738ded7818878a
neoxia <mbk@gmail.com>
2024-01-01 17:50:42 +0100

    not rebuilding postgres if already

--- start_back.sh
+++ start_back.sh
@@ -3,4 +3,8 @@ NETWORK_NAME=unionistashop_network
 if [ -z $(docker network ls --filter name=^${NETWORK_NAME}$ --format="{{ .Name }}") ] ; then 
      docker network create --driver bridge ${NETWORK_NAME} ; 
 fi
-docker compose --file /srv/back/current/docker-compose.yml up
+if [ -z $(docker ps --filter name=postgres --format="{{ .Names }}" )]; then 
+     docker compose --file /srv/back/current/docker-compose.yml up --build
+else 
+    docker compose --file /srv/back/current/docker-compose.yml --profile static up --build
+fi
