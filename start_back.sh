c383c9aa139e6cc716d9179ef8f363788e712eaf
neoxia <mbk@gmail.com>
2024-01-01 18:12:15 +0100

    start static if

--- start_back.sh
+++ start_back.sh
@@ -3,8 +3,8 @@ NETWORK_NAME=unionistashop_network
 if [ -z $(docker network ls --filter name=^${NETWORK_NAME}$ --format="{{ .Name }}") ] ; then 
      docker network create --driver bridge ${NETWORK_NAME} ; 
 fi
-if [ -z $(docker ps --filter name=postgres --format="{{ .Names }}" )]; then 
-     docker compose --file /srv/back/current/docker-compose.yml up --build
+if [ -z $(docker ps --filter name=postgres --format="{{ .Names }}") ] ; then 
+     docker compose --file /srv/back/current/docker-compose.yml --profile static up --build
 else 
-    docker compose --file /srv/back/current/docker-compose.yml --profile static up --build
+     docker compose --file /srv/back/current/docker-compose.yml up --build
 fi
