894ca71adec15d8028c74876911141527839d7e7
neoxia <mbk@gmail.com>
2024-01-02 08:44:33 +0100

    removing volumes each time

--- start_back.sh
+++ start_back.sh
@@ -1,4 +1,6 @@
 docker compose --file /srv/back/current/docker-compose.yml down
+VOLUMES=$(docker volume ls | awk 'NR>1{print $2}' | awk 'BEGIN { ORS = " " } { print }');
+docker volume rm $VOLUMES;
 NETWORK_NAME=unionistashop_network
 if [ -z $(docker network ls --filter name=^${NETWORK_NAME}$ --format="{{ .Name }}") ] ; then 
      docker network create --driver bridge ${NETWORK_NAME} ; 
