218150be76c949172c2e2416d7ec60de4647fd0a
mbenkhouya <mbenkhouya@fr-d7d40c2hyg.home>
2024-01-21 19:04:22 +0100

    commented start_back

--- start_back.sh
+++ start_back.sh
@@ -1,10 +1,16 @@
+
 docker compose --file /srv/back/current/docker-compose.yml down
+# remove existed volumes
 VOLUMES=$(docker volume ls | awk 'NR>1{print $2}' | awk 'BEGIN { ORS = " " } { print }');
 docker volume rm $VOLUMES;
+
+# create network(between front & back) if it does not exist already.
 NETWORK_NAME=unionistashop_network
 if [ -z $(docker network ls --filter name=^${NETWORK_NAME}$ --format="{{ .Name }}") ] ; then 
      docker network create --driver bridge ${NETWORK_NAME} ; 
 fi
+
+# rebuild db only if do not it exist already
 if [ -z $(docker ps --filter name=postgres --format="{{ .Names }}") ] ; then 
      docker compose --file /srv/back/current/docker-compose.yml down --remove-orphans
      docker compose --file /srv/back/current/docker-compose.yml --profile static up --build
