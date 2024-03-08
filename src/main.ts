8d11faa23f8dff3a3bacd99dc25a992541fcdb04
mbenkhouya <mbenkhouya@fr-d7d40c2hyg.home>
2024-03-08 18:12:18 +0100

    run migrations after start

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { createConnection } from 'typeorm';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.BACK_PORT || 3000);
+
+  const connection = await createConnection({
+    type: 'postgres',
+    host: process.env.TYPEORM_HOST || 'localhost',
+    port: parseInt(process.env.TYPEORM_PORT),
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
+    entities: ['src/**/*.model.ts'],
+    migrations: ['dist/scripts/migrations/*'],
+    logging: true,
+  });
+  await connection.runMigrations({
+    transaction: 'all'
+  });
+  await connection.close();
 }
 bootstrap();
